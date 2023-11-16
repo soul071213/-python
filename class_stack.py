ec6f0e6ba23705db17f2ac72f1aeead8081a1b6c
soul071213 <js071213asd@gmail.com~>
2023-11-16 16:07:10 +0900

    class_stack

--- class_stack.py
+++ class_stack.py
@@ -1,47 +1,51 @@
-stack_size=5
-top=-1
-list=[None]*stack_size
-
-def push(a):
-
-    if not isFull(): 
-        global top
-        top=top+1
-        list[top]=a 
-    else:
-        pass
-def pop():
-    if not isEmpty():
-        global top
-        top=top-1
-        return list[top+1] 
-    else:
-        pass
-def isEmpty():
-    if(top==-1):
-        return True
-    else:
+class ii:
+    def __init__(self):
+        self.max_size=5
+        self.arr1=[None] * self.max_size
+        self.top=-1
+class stack(ii):
+    def __init__(self,ip):
+        super().__init__()
+        self.ip=ip
+    def push(self):
+       if not self.isFull():
+            self.top+=1
+            self.arr1[self.top]=self.ip
+    def isEmpty(self):
+        if(self.top==-1):
+            return True
         return False
-def isFull():
-    if(top==stack_size-1):
-        return True
-    else:
+    def isFull(self):
+        if(self.top==self.max_size-1):
+            return True
         return False
-def peek():
-    if not isEmpty():
-        return list[top]
+    def pop(self):
+       if not self.isEmpty():
+            self.top-=1
+            return self.arr1[self.top+1]
+    def peak(self):
+       if not self.isEmpty():
+            return self.arr1[self.top]
+
+ii()
+while(1):
+    print("1은 push 2는 pop 3은 peak")
+    k=int(input())
+    if(k==1):
+        print("push 실행")
+        ip=int(input())
+        st1=stack(ip)
+        st1.push()
+
+    elif(k==2):
+        print("pop 실행")
+        print(st1.pop()) 
+
+    elif(k==3):
+        print("peak 실행")
+        print(st1.peak())
+
     else:
-        pass
-    
+         print("끝")
+         break
 
-push(1)
-push(2)
-push(3)
-push(4)
-push(5)
-push(6)
-
-print(peek())
-print(pop())
-print(pop())
-print(peek())
